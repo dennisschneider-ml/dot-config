938ed61722249fc060f77bd0d6159d273ad49898
Dennis Schneider <d.schneider.ulm@gmx.de>
2022-09-06 13:03:25 +0200

    FEAT: Automatic zen-mode reading

--- nvim/plugin/himalaya.lua
+++ nvim/plugin/himalaya.lua
@@ -1,11 +1,20 @@
 local truezen = require('true-zen')
+local tzfocus = require('true-zen.focus')
+local tzataraxis = require('true-zen.ataraxis')
 
 vim.g.himalaya_mailbox_picker = 'telescope'
 
--- read message in zen-mode
-vim.keymap.set('n', '<leader><cr>', function() 
-    vim.call('himalaya#msg#read')
-    truezen.focus()
-    truezen.ataraxis() 
+-- Open message in zen-mode
+-- Close on <Leader><CR>
+vim.keymap.set('n', '<Leader><CR>', function() 
+    if not tzfocus.running and not tzataraxis.running then
+        vim.call('himalaya#msg#read')
+        tzfocus.on()
+        tzataraxis.on()
+    else
+        tzataraxis.off()
+        tzfocus.off()
+        vim.api.nvim_command('quit')
+    end
 end, {noremap=true})
 
