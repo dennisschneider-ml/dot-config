b717a9da92d784380ace9bc23d191f56c07b6b24
Dennis Schneider <d.schneider.ulm@gmx.de>
2022-10-27 13:33:30 +0200

    FEAT: Minimal statusbar

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -1,1 +1,22 @@
-require('lualine').setup()
+require('lualine').setup({
+    options = {
+        theme = 'nord',
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {
+            {
+                'diagnostics',
+                sources = {'coc'},
+            }
+        },
+        lualine_y = {},
+        lualine_z = {},
+    },
+    extensions = {
+        'fzf',
+        'nvim-tree',
+    },
+})
