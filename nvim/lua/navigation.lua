1d21f430058d1a6d827afc5cd5815c1a755e42e8
Dennis Schneider <d.schneider.ulm@gmx.de>
2023-01-05 00:17:55 +0100

    FEAT: Window maneuvering

--- nvim/lua/navigation.lua
+++ nvim/lua/navigation.lua
@@ -1,5 +1,6 @@
 --[[ nvim autoenables incsearch, hlsearch
 --]]
+local expr_opts = {noremap = true}
 
 -- Enable use of mouse in all vim-modes
 vim.opt.mouse = 'a'
@@ -9,3 +10,49 @@ vim.keymap.set('n', '<C-H>', '<C-W>h', expr_opts)
 vim.keymap.set('n', '<C-J>', '<C-W>j', expr_opts)
 vim.keymap.set('n', '<C-K>', '<C-W>k', expr_opts)
 vim.keymap.set('n', '<C-L>', '<C-W>l', expr_opts)
+
+-- window-split resize
+vim.keymap.set(
+    'n', 
+    '<C-S-H>', 
+    function()
+        local curr_width = vim.api.nvim_win_get_width(0)
+        vim.api.nvim_win_set_width(0, curr_width+2)
+    end,
+    expr_opts)
+
+vim.keymap.set(
+    'n', 
+    '<C-S-J>', 
+    function()
+        local curr_height = vim.api.nvim_win_get_height(0)
+        vim.api.nvim_win_set_height(0, curr_height-2)
+    end,
+    expr_opts)
+
+vim.keymap.set(
+    'n', 
+    '<C-S-K>', 
+    function()
+        local curr_height = vim.api.nvim_win_get_height(0)
+        vim.api.nvim_win_set_height(0, curr_height+2)
+    end,
+    expr_opts)
+
+vim.keymap.set(
+    'n', 
+    '<C-S-L>', 
+    function()
+        local curr_width = vim.api.nvim_win_get_width(0)
+        vim.api.nvim_win_set_width(0, curr_width-2)
+    end,
+    expr_opts)
+
+-- window-tab conversion
+-- window to tab
+vim.keymap.set(
+    'n', 
+    '<C-T>', 
+    '<C-W>T',
+    expr_opts)
+
