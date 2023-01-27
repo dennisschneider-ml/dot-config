19cc6ab8b5ae3affa0b02ab83a4cac6f38aa6ebf
Dennis Schneider <d.schneider.ulm@gmx.de>
2023-01-27 22:20:15 +0100

    FIX: Only show when necessary

--- nvim/plugin/bufferline.lua
+++ nvim/plugin/bufferline.lua
@@ -2,7 +2,7 @@ require('bufferline').setup{
     options = {
         mode = 'tabs',
         diagnostics = 'coc',
-        always_show_bufferline = true,
+        always_show_bufferline = false,
         offsets = {
             {
                 filetype = "NvimTree", text = "", padding = 1
