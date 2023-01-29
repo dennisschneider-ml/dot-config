16c3e5d2ca55c1ee7715c8bc4635588004e3ee8c
Dennis Schneider <d.schneider.ulm@gmx.de>
2023-01-29 23:35:22 +0100

    FIX: Browser Music-Download works

--- spectrwm/scripts/download_music.sh
+++ spectrwm/scripts/download_music.sh
@@ -1,34 +1,33 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 function is_browser_window() {
     local window_id=$1
-    local browser_identifier=$2
+    local browser_name=$2
 
-    local browser_ids=$(xdotool search --class $browser_identifier)
-    return "(echo $browser_ids | grep -w -q $window_id)"
+    local browser_ids=$(xdotool search --class $browser_name)
+    return $(echo $browser_ids | grep -qm1 $window_id)
 }
 
-# Get the window ID of the active Chrome window
+# Get the window ID of the active Browser window
 active_window_id=$(xdotool getactivewindow)
-is_browser_window $active_window_id 'Brave-browser' 
-dunstify "$(echo $?)"
 
-if [ $(is_browser_window $active_window_id 'Brave-browser') ]; then
-    dunstify "heeeeee"
+if is_browser_window $active_window_id 'Brave-browser'; then
     old_cache=$(xclip -o)
     xdotool key --clearmodifiers --window $active_window_id 'ctrl+l+c'
     url=$(xclip -o)
-    if [ $url == *'youtube.'* ]; then
-        dunstify $url
-        echo $old_cache | xclip -o -selection primary
+    if [[ $url == *'youtube.'* ]]; then
+        echo $old_cache | xclip -o -selection clipboard
         info=$(zenity --forms --title "File information" --add-entry "Artist:" --add-entry "Album:")
-        artist=$(echo $info | cut -d'|' -f1 | tr ' ' '-')
-        album=$(echo $info | cut -d'|' -f2 | tr ' ' '-')
+        is_cancelled=$?
+        if [[ $is_cancelled -eq 0 ]]; then
+            artist=$(echo $info | cut -d'|' -f1 | tr ' ' '-')
+            album=$(echo $info | cut -d'|' -f2 | tr ' ' '-')
 
-        mkdir -p "Music/$artist/$album"
-        dunstify "Downloading to Music/$artist/$album..."
-        youtube-dl -x --audio-format opus --audio-quality 0 --prefer-ffmpeg --no-playlist -o "Music/$artist/$album/%(title)s.%(ext)s" $url
-        dunstify "Finished downloading music."
+            mkdir -p "Music/$artist/$album"
+            dunstify "Downloading to Music/$artist/$album..."
+            youtube-dl -x --audio-format opus --audio-quality 0 --prefer-ffmpeg --no-playlist -o "Music/$artist/$album/%(title)s.%(ext)s" $url
+            dunstify "Finished downloading music."
+        fi
     fi
 fi
 
