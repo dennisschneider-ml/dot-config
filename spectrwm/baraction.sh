e10fb2df7c1805a682af73ba514fc8193dfd7e53
Dennis Schneider <d.schneider.ulm@gmx.de>
2023-02-21 11:05:49 +0100

    FEAT: Added dennisschneider-ml/min Minimization Manager

--- spectrwm/baraction.sh
+++ spectrwm/baraction.sh
@@ -6,43 +6,37 @@ wifi() {
     echo "$WIFI"
 }
 
-mail() {
-    NEW_MAIL=$(claws-mail --status | head -1 | awk '{print $1}')
-    echo "Mail: $NEW_MAIL"
-}
-
 mem() {
     MEM=$(free -h | awk '/Mem:/{print $3}')
-    echo "Mem: $MEM"
+    echo "󰍛 $MEM"
 }
 
 battery() {
     BAT=$(cat /sys/class/power_supply/BAT0/capacity)
-    echo "Bat: $BAT%"
+    echo "  $BAT%"
 }
 
 cpu_temp() {
     TEMP=$(sensors | awk '/CPU/{print substr($2,2)}')
-    echo "CPU: $TEMP"
+    echo " $TEMP"
 }
 
 sound_device() {
     SOUND=$(pactl list | grep -m 1 device.alias)
     if [[ $SOUND ]]; then
-        SOUND_ICON=''
+        SOUND_ICON='󰋎'
     fi
     echo "$SOUND_ICON"
 }
 
 minimized() {
-    current_workspace=$(xdotool get_desktop)
-    num_minimized=$(awk -v wks=$current_workspace -F ';' '$3~wks{print $1}' $PYNNACLE_DATA/minimized_programs.txt | wc -l)
+    num_minimized=$(min-list | sed '/^$/D' | wc -l)
     if [[ $num_minimized != 0 ]]; then
-        echo "~$num_minimized"
+        echo "󰻀 $num_minimized"
     fi
 }
 
 while :; do
-    echo "$(sound_device)        $(wifi) | $(battery) | $(cpu_temp) | $(mem) |"
+    echo "$(sound_device)   $(minimized)   $(wifi)   $(battery)   $(cpu_temp)   $(mem) |"
     sleep 1
 done
--- spectrwm/scripts/unminimize.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-current_workspace=$(xdotool get_desktop)
-chosen=$(awk -v wks=$current_workspace -F ';' '$3~wks{print $1}' $PYNNACLE_DATA/minimized_programs.txt | rofi -dmenu -i)
-chosen_id=$(grep $chosen $PYNNACLE_DATA/minimized_programs.txt | head -1 | cut -d';' -f2)
-xdotool windowmap "$chosen_id"
-sed -i "/$chosen_id/d" $PYNNACLE_DATA/minimized_programs.txt
-
