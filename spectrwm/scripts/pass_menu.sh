65ee3af45054598831c661c3dfab3027b864ba76
Dennis Schneider <d.schneider.ulm@gmx.de>
2023-03-20 19:20:51 +0100

    FEAT: Insert Username and password

--- spectrwm/scripts/pass_menu.sh
+++ spectrwm/scripts/pass_menu.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 chosen=$(ls .local/share/password-store -I 'otp_*' | cut -d. -f1 | rofi -dmenu -i)
-pass -c $chosen
+content=$(pass $chosen)
+password=$(echo "$content" | sed -n 1p)
+username=$(echo "$content" | sed -n '2s/Username: //p')
+
+echo "$password" | xclip -selection clip
+echo "$username" | xclip -selection primary
+
