0b8062a50160d3967fd992470a27c0344e24ec18
Dennis Schneider <d.schneider.ulm@gmx.de>
2023-06-07 17:53:20 +0200

    feat: Show notifications on screenshot

---
+++ nvim/plugin/neogen.lua
@@ -0,0 +1,15 @@
+local neogen = require('neogen')
+
+neogen.setup({
+    snippet_engine="snippy",
+    languages = {
+        python = {
+            template = {
+                annotation_convention = "numpydoc",
+            }
+        }
+    }
+})
+
+vim.keymap.set('n', '<leader>cc', neogen.generate, {noremap=true, silent=true})
+
