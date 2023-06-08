3d4e74b6c03d53bb8cd3a677edb348fead4bdde1
Dennis Schneider <d.schneider.ulm@gmx.de>
2023-06-08 08:53:01 +0200

    fix: Hide bufferline on zen-mode

--- nvim/plugin/true-zen.lua
+++ nvim/plugin/true-zen.lua
@@ -3,26 +3,41 @@ local expr_opts = { noremap = true, silent = true }
 
 truezen.setup({
     modes = { -- configurations per mode
-		ataraxis = {
-			minimum_writing_area = { -- minimum size of main window
-				width = 80,
-				height = 44,
-			},
-			padding = { -- padding windows
-				left = 52,
-				right = 52,
-				top = 0,
-				bottom = 0,
-			},
-		},
-	},
-	integrations = {
-		twilight = true, -- enable twilight (ataraxis)
-		lualine = true -- hide nvim-lualine (ataraxis)
-	},
+        ataraxis = {
+            minimum_writing_area = { -- minimum size of main window
+                width = 80,
+                height = 44,
+            },
+            padding = { -- padding windows
+                left = 52,
+                right = 52,
+                top = 0,
+                bottom = 0,
+            },
+            callbacks = {
+                -- Make bufferline disappear when entering Zen-Mode.
+                open_pos = function()
+                    vim.cmd("set showtabline=0")
+                end,
+                close_pos = function()
+                    vim.cmd("set showtabline=2")
+                end
+            }
+        },
+    },
+    integrations = {
+        lualine = true -- hide nvim-lualine (ataraxis)
+    },
 })
 
 
-vim.keymap.set('n', '<leader>zf', truezen.focus, expr_opts)
-vim.keymap.set('n', '<leader>zm', truezen.minimalist, expr_opts)
-vim.keymap.set('n', '<leader>zz', truezen.ataraxis, expr_opts)
+local wk = require("which-key")
+
+wk.register({
+    z = {
+        name = "zen",
+        f = { truezen.focus, "Focus Window", expr_opts },
+        m = { truezen.minimalist, "Minimalist Mode", expr_opts },
+        z = { truezen.ataraxis, "Zen Mode", expr_opts },
+    }
+}, { prefix = "<leader>" })
