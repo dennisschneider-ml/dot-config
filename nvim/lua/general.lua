10b998fa3efd0007eb84f911e6feccd9a93ff2dd
Dennis Schneider <d.schneider.ulm@gmx.de>
2023-06-19 21:18:14 +0200

    fix: Execute command

--- nvim/lua/general.lua
+++ nvim/lua/general.lua
@@ -25,5 +25,5 @@ vim.cmd([[
 
 -- Note: Running the check 1e6 times takes 1.1 seconds.
 -- Execute current file.
-vim.keymap.set('n', '<leader>o', ':wa | !([ -x % ] || chmod +x %) & %', {desc = "Execute file"})
+vim.keymap.set('n', '<leader>o', ':wa | !([ -x % ] || chmod +x %) & ./%<CR>', {desc = "Execute file"})
 
