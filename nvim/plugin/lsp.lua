7da48bd8d4150c9145b61c1b2250bed4b392cd80
Dennis N. Schneider <d.schneider.ulm@gmx.de>
2023-09-19 19:26:31 +0200

    feat: Added cmp and working integration

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -40,102 +40,103 @@ local on_attach = function(client, bufnr)
     bufmap('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
     -- Show diagnostics in a floating window
     bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-    -- Move to the previous diagnostic
+    -- Move to the previous diagnostic-
     bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
     -- Move to the next diagnostic
     bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
 end
 
 lspconfig.ruff_lsp.setup {
-    on_attach = on_attach
+    on_attach = on_attach,
+    root_dir = require('lspconfig').util.find_git_ancestor
 }
 
 -- cmp
---vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
---local cmp = require('cmp')
---
---local select_opts = {behavior = cmp.SelectBehavior.Select}
---
---cmp.setup({
---    snippet = {
---        expand = function(args)
---            require('snippy').expand_snippet(args.body)
---        end
---    },
---
---    sources = { -- Sources for completion.
---      {name = 'snippy' }, -- Available snippets.
---      {name = 'nvim_lsp_signature_help'},
---      {name = 'nvim_lsp'},
---      {name = 'path'}, -- Autocomplete file paths.
---      {name = 'buffer'}, -- Words found in current buffer.
---    },
---
---    -- Controls the appearance and settings for the documentation window.
---    window = {
---        documentation = cmp.config.window.bordered()
---    },
---
---    experimental = {
---        ghost_text = true,
---    },
---
---    formatting = {
---        fields = {"abbr", "menu"},
---        format = function(entry, item)
---            local menu_icon = {
---                nvim_lsp = ' ',
---                snippy = '󰁨 ',
---                buffer = ' ',
---                path = ' ',
---                nvim_lsp_signature_help = " ",
---            }
---
---            item.menu = menu_icon[entry.source.name]
---            return item
---        end,
---    },
---
---    mapping = {
---        ['<C-Space>'] = cmp.mapping.complete(),
---        ['<CR>'] = cmp.mapping.confirm({select = true}),
---        ['<C-k>'] = cmp.mapping.select_prev_item(select_opts),
---        ['<C-j>'] = cmp.mapping.select_next_item(select_opts),
---        ['<Tab>'] = cmp.mapping(function(fallback)
---            if cmp.visible() then
---                cmp.select_next_item(select_opts)
---            elseif snippy ~= nil then
---                if snippy.expandable() then
---                    snippy.expand()
---                elseif snippy.can_expand_or_advance() then
---                    snippy.expand_or_advance()
---                else
---                    fallback()
---                end
---            else
---                fallback()
---            end
---        end, {'i', 's'}),
---    }
---})
---
---cmp.setup.cmdline({'/', '?'}, {
---    mapping = cmp.mapping.preset.cmdline(),
---    sources = {
---        { name = "buffer" }
---    }
---})
---
---cmp.setup.cmdline({':'}, {
---    mapping = cmp.mapping.preset.cmdline(),
---    sources = {
---        { name = "path" }
---    }, {
---        { name = "cmdline"}
---    }
---})
---
---
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+local cmp = require('cmp')
+
+local select_opts = {behavior = cmp.SelectBehavior.Select}
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require('snippy').expand_snippet(args.body)
+        end
+    },
+
+    sources = { -- Sources for completion.
+      {name = 'snippy' }, -- Available snippets.
+      {name = 'nvim_lsp_signature_help'},
+      {name = 'nvim_lsp'},
+      {name = 'path'}, -- Autocomplete file paths.
+      {name = 'buffer'}, -- Words found in current buffer.
+    },
+
+    -- Controls the appearance and settings for the documentation window.
+    window = {
+        documentation = cmp.config.window.bordered()
+    },
+
+    experimental = {
+        ghost_text = true,
+    },
+
+    formatting = {
+        fields = {"abbr", "menu"},
+        format = function(entry, item)
+            local menu_icon = {
+                nvim_lsp = ' ',
+                snippy = '󰁨 ',
+                buffer = ' ',
+                path = ' ',
+                nvim_lsp_signature_help = " ",
+            }
+
+            item.menu = menu_icon[entry.source.name]
+            return item
+        end,
+    },
+
+    mapping = {
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({select = true}),
+        ['<C-k>'] = cmp.mapping.select_prev_item(select_opts),
+        ['<C-j>'] = cmp.mapping.select_next_item(select_opts),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item(select_opts)
+            elseif snippy ~= nil then
+                if snippy.expandable() then
+                    snippy.expand()
+                elseif snippy.can_expand_or_advance() then
+                    snippy.expand_or_advance()
+                else
+                    fallback()
+                end
+            else
+                fallback()
+            end
+        end, {'i', 's'}),
+    }
+})
+
+cmp.setup.cmdline({'/', '?'}, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = "buffer" }
+    }
+})
+
+cmp.setup.cmdline({':'}, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = "path" }
+    }, {
+        { name = "cmdline"}
+    }
+})
+
+
 local sign = function(opts)
     vim.fn.sign_define(opts.name, {
         texthl = opts.name,
