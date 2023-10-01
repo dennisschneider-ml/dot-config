29f321857e5c490cf097a414ffe1654b26e7a9b9
Dennis N. Schneider <d.schneider.ulm@gmx.de>
2023-10-01 17:46:36 +0200

    fix: Incorrect CR handling

--- nvim/plugin/common.lua
+++ nvim/plugin/common.lua
@@ -8,18 +8,5 @@ local npairs_loaded, npairs = pcall(require, 'nvim-autopairs')
 local cmp_loaded, cmp = pcall(require, 'cmp')
 
 
--- plugin-common <CR> handling
-local function cr_handling()
-    --if vim.call('coc#pum#visible') == 1 then
-    --    return vim.call('coc#pum#confirm')
-    --elseif npairs_loaded then
-    if not cmp.visible() then
-        if npairs_loaded then
-            return npairs.autopairs_cr()
-        else
-            return '<CR>'
-        end
-    end
-end
 
-vim.keymap.set('i', '<CR>', cr_handling, { expr = true, noremap = true })
+--  vim.keymap.set('i', '<CR>', cr_handling, { expr = true, noremap = true })
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -110,7 +110,7 @@ _G.packer_plugins = {
     url = "https://github.com/dcampos/cmp-snippy"
   },
   ["dracula.nvim"] = {
-    config = { "\27LJ\2\n\\\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\19italic_comment\2\19transparent_bg\2\nsetup\fdracula\frequire\0" },
+    config = { "\27LJ\2\n\\\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\19transparent_bg\2\19italic_comment\2\nsetup\fdracula\frequire\0" },
     loaded = true,
     path = "/home/dns/.local/share/nvim/site/pack/packer/start/dracula.nvim",
     url = "https://github.com/Mofiqul/dracula.nvim"
@@ -275,7 +275,7 @@ _G.packer_plugins = {
 time([[Defining packer_plugins]], false)
 -- Config for: dracula.nvim
 time([[Config for dracula.nvim]], true)
-try_loadstring("\27LJ\2\n\\\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\19italic_comment\2\19transparent_bg\2\nsetup\fdracula\frequire\0", "config", "dracula.nvim")
+try_loadstring("\27LJ\2\n\\\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\2\19transparent_bg\2\19italic_comment\2\nsetup\fdracula\frequire\0", "config", "dracula.nvim")
 time([[Config for dracula.nvim]], false)
 
 _G._packer.inside_compile = false
